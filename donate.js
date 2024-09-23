f762ed76ac9578db3ac9ba97636e1a22a147087c
shafriki <shafriki.cse@gmail.com>
2024-09-23 15:59:07 +0600

    added js donation, history part

--- donate.js
+++ donate.js
@@ -2,7 +2,36 @@
 const htmlTag = document.getElementsByTagName("html")[0];
 htmlTag.setAttribute("data-theme", "light");
 
-// Get Element by ID function
+//id function
 function id(id) {
   return document.getElementById(id);
 }
+
+// main balance
+let balance = 5500.55; 
+const balanceElement = id("balance");
+balanceElement.textContent = balance.toFixed(2); 
+
+// donation, history button declared 
+const donationButton = id("donation");
+const historyButton = id("history");
+let donationHistory = [];
+
+// donation section
+donationButton.addEventListener("click", function () {
+  id("history-section").classList.add("hidden"); 
+  id("thanks-modal").classList.add("hidden");
+  document.querySelector("main").style.display = "block";
+
+  this.classList.add("bg-[#B4F461]", "text-white");
+  historyButton.classList.remove("bg-[#B4F461]", "text-white");
+});
+
+// Show history section
+historyButton.addEventListener("click", function () {
+  id("history-section").classList.remove("hidden"); 
+  document.querySelector("main").style.display = "none"; 
+
+  this.classList.add("bg-[#B4F461]", "text-white");
+  donationButton.classList.remove("bg-[#B4F461]", "text-white");
+});
