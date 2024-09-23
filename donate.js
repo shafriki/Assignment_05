d596f08211f627747fd6320f07fdd8af2e015f4a
shafriki <shafriki.cse@gmail.com>
2024-09-23 15:30:28 +0600

    design section complete

--- donate.js
+++ donate.js
@@ -1,5 +1,8 @@
-// dark to light mood
+// Dark to light mode
 const htmlTag = document.getElementsByTagName("html")[0];
 htmlTag.setAttribute("data-theme", "light");
 
-console.log("donate bangladesh")
+// Get Element by ID function
+function id(id) {
+  return document.getElementById(id);
+}
